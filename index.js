6ed8c8cefccb6e0b8130a54cc8afe07bd1a10978
Earlyflash <17590628+Earlyflash@users.noreply.github.com>
2025-07-21 23:43:59 +0000

    idnex

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-export async function fetch(request) {
-  // Proxy the Datadog status API response
-  const response = await fetch('https://status.datadoghq.eu/api/v2/status.json');
+export default {
+  async fetch(request) {
+    const response = await fetch('https://status.datadoghq.eu/api/v2/status.json');
+    const data = await response.text();
 
-  // Get the response body text
-  const data = await response.text();
-
-  // Return response with CORS headers added
-  return new Response(data, {
-    status: response.status,
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type',
-    },
-  });
-}
+    return new Response(data, {
+      status: response.status,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
+    });
+  }
+};
